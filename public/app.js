1f3b752a4bd51ad27b3c12664b2c358cd8dad4c8
hinoja <hinoja2@gmail.com>
2022-12-24 22:54:13 +0100

    modif

---
+++ public/app.js
@@ -0,0 +1,15 @@
+import '../resources/js/bootstrap';
+
+alert('hello');
+// const nickname=document.getElementById("nickname");
+// const message=document.getElementById("message-chat");
+// const submitButton=document.getElementById("submitButton");
+//                     submitButton.addEventListener('click', () => {
+//                             axios.post('/chat',{
+//                                  nickname: nickname.value,
+//                                 message:message.value
+//                             });
+//                     });
+// window.Echo.channel('chat').listen('.chat-message',(event)=>{
+//   console.log(event);
+// });
